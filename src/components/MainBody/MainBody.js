ae5d3c7f2c6218c92e018368632e7dd9898e72f8
Abdul Aziz <63227100+AbdulAzizMiazi@users.noreply.github.com>
2020-08-27 17:49:02 +0600

    first commit

---
+++ src/components/MainBody/MainBody.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const MainBody = () => {
+    return (
+        <div>
+            <h1>I am here</h1>
+        </div>
+    );
+};
+
+export default MainBody;
---
+++ src/components/dataSource.js
@@ -0,0 +1,124 @@
+const dataSource = [
+    [
+        {
+            title: "Deep Learning",
+            category: "Data Science",
+            by: "deeplearning.ai",
+            price: 23030,
+            img: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fbernardmarr%2Ffiles%2F2018%2F10%2FAdobeStock_179912599-1-1200x797.jpg"
+        },
+        {
+            title: "Data Science",
+            category: "Data Science",
+            by: "Johns Hopkins University",
+            price: 21700,
+            img: "https://www.copyright.com/wp-content/uploads/2017/11/enterprise-data-science.png"
+        },
+        {
+            title: "Data Science: Foundations using R",
+            category: "Data Science",
+            by: "Johns Hopkins University",
+            price: 15093.99,
+            img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/5e/b4ef8069b511e3ae92c39913bb30e0/Rprogramming.jpg?auto=format%2Ccompress&dpr=1"
+        },
+        {
+            title: "IBM Data Science",
+            category: "Data Science",
+            by: "IBM",
+            price: 32730,
+            img: "https://blog.edx.org/wp-content/uploads/2020/02/data-science_IBM-blog.png"
+        },
+        {
+            title: "Machine Learning",
+            category: "Data Science",
+            by: "Stanford University",
+            price: 19050.73,
+            img : "https://s3.amazonaws.com/prod-www-blackline-com/blog/wp-content/uploads/2019/01/29163118/A-Conversation-With-BlackLines-Machine-Learning-Experts-720x405.jpg"
+        },
+        {
+            title: "Applied Data Science with Python",
+            category: "Data Science",
+            by: "University Of Michigan",
+            price: 19030,
+            img: "https://i2.wp.com/sanitsolutions.in/wp-content/uploads/2018/08/Machine-learning-Training-in-Bangalore.jpg?w=758"
+        },
+        {
+            title: "Excel to MySQL: Analytic Techniques for Business",
+            category: "Data Science",
+            by: "Duke University",
+            price: 11090,
+            img: "https://1.bp.blogspot.com/-w25SnXLC-QA/WGdwtQtNHHI/AAAAAAAAAWw/lF4pQJ0xvoAoYjjqh3JGi7ePf2OTqxpegCLcB/s1600/how-to-export-excel-data-into-mysql-database-using-php.png"
+        },
+        {
+            title: "Introduction to Data Science",
+            category: "Data Science",
+            by: "IBM",
+            price: 27500,
+            img: "https://cdn.educba.com/academy/wp-content/uploads/2019/03/Introduction-To-Data-Science.jpg"
+        },
+    ],
+    [
+        {
+            title: "Global Master of Business Administration (GMBA)",
+            category: "Business",
+            by: "Macquarie University",
+            price: 33000,
+            img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera_assets/ddp/branding/global-mba-macquarie/banner.jpg?auto=format%2Ccompress&dpr=1&w=300&h=&fit=crop"
+        },
+        {
+            title: "Master of Business Administration",
+            category: "Business",
+            by: "University of Illinois",
+            price: 21744,
+            img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera_assets.s3.amazonaws.com/growth_certificate_degree/degree_background_image/Illinois_cropped.png?auto=format%2Ccompress&dpr=1&w=300&h=&fit=crop"
+        },
+        {
+            title: "Spatial Data Analysis and Visualization MasterTrack™ Program",
+            category: "Business",
+            by: " University of California",
+            price: 3000,
+            img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera_assets/degree-landing-pages/masters-innovation-entrepreneurship/croplogo.png?auto=format%2Ccompress&dpr=1&w=300&h=&fit=crop"
+        },
+        {
+            title: "Supply Chain Excellence MasterTrack™ Certificate",
+            category: "Business",
+            by: "Rutgers University",
+            price: 2000,
+            img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera_assets/ddp/branding/rutgers/supply-chain-excellence-rutgers-banner.png?auto=format%2Ccompress&dpr=1&w=300&h=&fit=crop"
+        }
+    ],
+    [
+        {
+            title: "Master of Public Health",
+            category: "Social Sciences",
+            by: "University of Michigan",
+            price: 96096,
+            img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera_assets/ddp/branding/umich/social-work-umich-banner.jpg?auto=format%2Ccompress&dpr=1&w=300&h=&fit=crop"
+        },
+        {
+            title: "Instructional Design MasterTrack™ Certificate",
+            category: "Social Sciences",
+            by: "University of Illinois",
+            price: 2384,
+            img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera_assets/ddp/branding/illinois/instructional-design-illinois-banner.jpg?auto=format%2Ccompress&dpr=1&w=300&h=&fit=crop"
+        },
+        {
+            title: "Social Work: Practice, Policy and Research MasterTrack™ Certificate",
+            category: "Social Sciences",
+            by: " University of Michigan",
+            price: 2000,
+            img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera_assets/ddp/branding/umich/social-work-umich-banner.jpg?auto=format%2Ccompress&dpr=1&w=300&h=&fit=crop"
+        },
+        {
+            title: "Instructional Design MasterTrack™ Certificate",
+            category: "Social Sciences",
+            by: "University of Illinois",
+            price: 2384,
+            img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera_assets/ddp/branding/illinois/instructional-design-illinois-banner.jpg?auto=format%2Ccompress&dpr=1&w=300&h=&fit=crop"
+        }
+    ]
+    
+]
+
+
+export default dataSource;
